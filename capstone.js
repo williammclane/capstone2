fa71a9043635bd77a1bd8e9d31a3708630ede66e
William McLane <dukeoftinmen@gmail.com>
2017-11-14 02:29:32 +0000

    ball works

--- capstone.js
+++ capstone.js
@@ -1,5 +1,11 @@
 var player = document.getElementById('player');
 var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
 var xPos = 0;
 var yPos = 0;
@@ -10,31 +16,56 @@ function move(e){
     if(e.keyCode == 39){
         xPos+=10;
         player.style.left = xPos + 'px';
-        if(xPos>=790){
+        if(xPos>=793){
             xPos-=10
         }
     }
     if(e.keyCode == 37){
         xPos-=10;
         player.style.left = xPos + 'px';
-        if(xPos<=0){
+        if(xPos<=10){
             xPos+=10
         }
     }
     if(e.keyCode == 38){
         yPos-=10;
         player.style.top = yPos +'px';
-        if(yPos<=0){
+        if(yPos<=10){
             yPos+=10
         }
     }
     if(e.keyCode == 40){
         yPos+=10;
         player.style.top = yPos +'px';
-        if(yPos>=490){
+        if(yPos>=493){
             yPos-=10
         }
     }
 }
 
 document.onkeydown = move;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
